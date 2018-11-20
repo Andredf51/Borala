6b64f4e6d6d073271e2ac5a2b53d8ddea3106544
Douglas <douglas.shibata11@gmail.com>
2018-11-20 10:14:28 -0200

    Hora da carona

--- src/pages/procurar/procurar.ts
+++ src/pages/procurar/procurar.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
 
 /**
  * Generated class for the ProcurarPage page.
@@ -15,11 +15,37 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class ProcurarPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  /*constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }*/
+  constructor(public alerCtrl: AlertController) { }
+
+  doConfirm() {
+    let confirm = this.alerCtrl.create({
+      title: 'Pode Confirmar a carona?',
+      message: 'Você está de acordo com o destino e o horário de saída?',
+      buttons: [
+        {
+          text: 'Discordo',
+          handler: () => {
+            console.log('Discordo clicked');
+          }
+        },
+        {
+          text: 'Concordo',
+          handler: () => {
+            console.log('Concordo clicked');
+          }
+        }
+      ]
+    });
+    confirm.present()
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad ProcurarPage');
+  }
+  public event = {
+    timeStarts: '00:00',
   }
 
 }
